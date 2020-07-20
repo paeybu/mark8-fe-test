d50a0766adeef019bc6cf8584a9e988fb63dc7e5
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2020-07-21 00:15:05 +0700

    add upload csv

---
+++ components/BulkUploadForm.js
@@ -0,0 +1,70 @@
+import { Card, Row, Col, Upload, Button } from 'antd';
+import { ExclamationOutlined, UploadOutlined } from '@ant-design/icons';
+import { CircleIconWrapper, RectangleIconWrapper } from './Icon';
+import Papa from 'papaparse';
+
+const BulkUploadForm = () => (
+  <Row justify='center'>
+    <Col span={12}>
+      <Card
+        title={
+          <>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <h2 style={{ marginBottom: 0 }}>
+                <strong>Bulk Upload Form</strong>
+              </h2>
+              <CircleIconWrapper>
+                <ExclamationOutlined style={{ color: 'white' }} />
+              </CircleIconWrapper>
+            </div>
+            <div>
+              <p style={{ color: '#0089ff' }}>
+                You haven't uploaded any bulk data yet
+              </p>
+            </div>
+          </>
+        }
+      >
+        <h2>
+          <strong>Choose your input method</strong>
+        </h2>
+        <Card>
+          <Row>
+            <Col>
+              <Upload
+                onChange={({ file, fileList }) => {
+                  file.status === 'done' &&
+                    Papa.parse(file.originFileObj, {
+                      complete: (res) => console.log(res),
+                      header: true,
+                    });
+                }}
+              >
+                <RectangleIconWrapper>
+                  <UploadOutlined
+                    style={{
+                      fontSize: ' 40px',
+                      color: '#002240',
+                      display: 'flex',
+                      width: '100%',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  />
+                </RectangleIconWrapper>
+              </Upload>
+            </Col>
+            <Col style={{ marginLeft: '15px' }}>
+              <p>
+                <strong>via CSV file</strong>
+              </p>
+              <p>อัปเดตข้อมูลจากไฟล์ CSV</p>
+            </Col>
+          </Row>
+        </Card>
+      </Card>
+    </Col>
+  </Row>
+);
+
+export default BulkUploadForm;
